b7f73523fd1c1cc97e292175beb24276b30f90a0
Lloyd Heartfire <beaudonloic@hotmail.com>
2025-07-11 10:03:49 +0200

    Realisators models, views, serializers done

--- back/app/settings.py
+++ back/app/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'rest_framework',
     'app.users',
     'app.movies',
+    'app.realisators',
 ]
 
 MIDDLEWARE = [
--- back/app/urls.py
+++ back/app/urls.py
@@ -18,10 +18,12 @@ from django.urls import include, path
 from rest_framework.routers import DefaultRouter
 from app.users.views import UserViewSet
 from app.movies.views import MovieViewSet
+from app.realisators.views import RealisatorViewSet
 
 router = DefaultRouter()
 router.register(r'users', UserViewSet)
 router.register(r'movies', MovieViewSet)
+router.register(r'realisators', RealisatorViewSet)
 
 urlpatterns = [
     path('', include(router.urls)),
